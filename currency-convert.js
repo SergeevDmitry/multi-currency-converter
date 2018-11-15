16256478d747bfe558ad2e4594ac63c67ef291c4
sergeevdms <sergeevdms@gmail.com>
2018-11-15 03:27:30 +0300

    last cahgnes

--- currency-convert.js
+++ currency-convert.js
@@ -1,34 +1,43 @@
 const axios = require('axios');
 
-// const getExchangeRate = (from, to) => {
-//   return axios.get('http://data.fixer.io/api/latest?access_key=527113fdba1be461eb20a589770073fe').then((response) => {
-//     const euro = 1 / response.data.rates[from];
-//     const rate = euro * response.data.rates[to];
-//     return rate;
-//   })
-// }
-
 const getExchangeRate = async (from, to) => {
-  const response = await axios.get('http://data.fixer.io/api/latest?access_key=527113fdba1be461eb20a589770073fe');
-  const euro = 1 / response.data.rates[from];
-  const rate = euro * response.data.rates[to];
-  return rate;
+  try {
+    const response = await axios.get('http://data.fixer.io/api/latest?access_key=527113fdba1be461eb20a589770073fe');
+    const euro = 1 / response.data.rates[from];
+    const rate = euro * response.data.rates[to];
+
+    if (isNaN(rate)) {
+      throw new Error();
+    }
+
+    return rate;
+  } catch (e) {
+    throw new Error(`Unable to get exchange rate for ${from} and ${to}`);
+  }
 }
 
 const getCountries = async (currencyCode) => {
-  const response = await axios.get(`https://restcountries.eu/rest/v2/currency/${currencyCode}`);
-  const countries = response.data.reduce((countriesArray, country) => {
-    countriesArray.push(country.name);
-    return countriesArray;
-  }, []);
+  try {
+    const response = await axios.get(`https://restcountries.eu/rest/v2/currency/${currencyCode}`);
+    const countries = response.data.reduce((countriesArray, country) => {
+      countriesArray.push(country.name);
+      return countriesArray;
+    }, []);
 
-  return countries;
+    return countries;
+  } catch (e) {
+    throw new Error(`Unable to get countries the use ${currencyCode}`);
+  }
 }
 
-getCountries('USD').then((countries) => {
-  console.log(countries);
-})
+const convertCurrency = async (from, to, amount) => {
+  const rate = await getExchangeRate(from, to);
+  const countries = await getCountries(to);
+  const convertedAmount = (amount * rate).toFixed(2);
 
-getExchangeRate('USD', 'CAD').then((rate) => {
-  console.log(rate);
-})
+  return `${convertedAmount} in ${countries.join(', ')}`;
+}
+
+convertCurrency('USD', 'CAD', 220)
+  .then((result) => console.log(result))
+  .catch((e) => console.log(e.message));
